fb45c1fc1925c7dae801dfdfd06716d339bcab29
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-05-15 22:53:15 -0400

    Initial Commit

---
+++ UUIDGenerator/UUIDGenerator.h
@@ -0,0 +1,13 @@
+//
+//  UUIDGenerator.h
+//  UUIDGenerator
+//
+//  Created by Jonathan Almeida on 2014-05-15.
+//  Copyright (c) 2014 Jonathan Almeida. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UUIDGenerator : NSObject
+
+@end
---
+++ UUIDGenerator/UUIDGenerator.m
@@ -0,0 +1,13 @@
+//
+//  UUIDGenerator.m
+//  UUIDGenerator
+//
+//  Created by Jonathan Almeida on 2014-05-15.
+//  Copyright (c) 2014 Jonathan Almeida. All rights reserved.
+//
+
+#import "UUIDGenerator.h"
+
+@implementation UUIDGenerator
+
+@end
